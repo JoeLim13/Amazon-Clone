587262ad6c3ec1301b20aa556977670627c25210
Joe Lim <joecounterzero@hotmail.com>
2021-02-20 12:07:11 +0800

    added checkout_page

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import React from 'react';
 import Header from './Header';
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from './Checkout';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header />
         <Switch>
+          <Route path="/login">
+            <h1>Sign In</h1>
+          </Route>
           <Route path="/checkout">
-            <Header />
-            <h1>Hello</h1>
+            <Checkout />
           </Route>
           <Route path="/">
-            <Header />
             <Home/>
           </Route>
         </Switch>
