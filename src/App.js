0587e4a1ca0554326c11f310be6824404492934c
Joe Lim <joecounterzero@hotmail.com>
2021-02-21 18:46:25 +0800

    added cloud based backend using firebase & payment authentication

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import Checkout from './Checkout';
 import Login from './Login';
 import { auth } from './firebase';
 import { useStateValue } from './StateProvider';
+import Payment from './Payment';
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe('pk_test_51INDSfIkf9zt2WM0ufXjf1RiZIRjWzcyyDVME44kquyVem3ahmCSJcHnuoBkIWQxmNdWqz2c1XsDY8Jdv41zQq4G00hrHXsXeO');
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -43,6 +48,12 @@ function App() {
           <Route path="/checkout">
               <Header />
             <Checkout />
+          </Route>
+          <Route path="/payment">
+              <Header />
+              <Elements stripe={promise}>
+                <Payment />
+              </Elements>
           </Route>
           <Route path="/">
               <Header />  
