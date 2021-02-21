38414b9c4b3ced6036bbd7ec446d933f7414c796
Joe Lim <joecounterzero@hotmail.com>
2021-02-21 15:15:54 +0800

    added user login/signup & authentication using firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyAVIVXFHJeYhPV5uAiL81xgnVQDuz1bcng",
     authDomain: "clone-16a0f.firebaseapp.com",
@@ -8,3 +11,10 @@ const firebaseConfig = {
     appId: "1:48515613013:web:5053be092059521b766fdc",
     measurementId: "G-SSHYWEJ97H"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
